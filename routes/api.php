5f4f1bba6fe18a72e2e19d72021e8d8156a7f284
Allrivenjs <69703004+Allrivenjs@users.noreply.github.com>
2022-10-09 21:16:09 -0500

    Update

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Phroute\Phroute\RouteCollector;
+$router = Config\Providers\RouteServiceProviders::getInstance()->getRouter();
+
+$router->get('/', function () {
+    echo 'Hello World!';
+});
+
+
+$router->group(['prefix'=> 'api'], function (RouteCollector $router){
+    $router->get('/login', 'App\Controllers\AuthController@login');
+
+    $router->group(['before' => 'auth'], function(RouteCollector $router){
+
+        $router->get('/posts', function(){
+            echo 'posts';
+        });
+
+    });
+
+});
+
+
+
+
