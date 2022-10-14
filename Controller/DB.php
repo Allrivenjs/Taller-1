fc61ce7e5509195a7b8a0990e2ac55b797b02b91
Sebastian <46248806+SebastianQuinchia@users.noreply.github.com>
2022-10-13 20:17:10 -0500

    up

--- Controller/DB.php
+++ Controller/DB.php
@@ -5,7 +5,7 @@ class DB
     private string $host = "localhost";
     private string $database = "deswebii";
     private static DB $instance;
-    private $connection;
+    private mysqli $connection; //u can't use mixed is crazy, don't try again... this it is a formal definition
 
     /**
      * @throws Exception
@@ -23,10 +23,10 @@ class DB
     /**
      * @throws PDOException
      */
-    private function ResetDefaultConnection($Rol): void
+    private function ResetDefaultConnection($Rol = null): void
     {
-        $UserDB='defaultuser';
-        $Pass='defaultuserpassword';
+        $UserDB='root';
+        $Pass='';
         if (isset($Rol)){
             /*
              * check if the variable exists, otherwise access is given with the default user.
@@ -39,6 +39,10 @@ class DB
         $this->connection = mysqli_connect($this->host,$UserDB, $Pass, $this->database);
     }
 
+    /**
+     * @param $Rol
+     * @return void
+     */
     public function changeConnectionRol($Rol):void{
         /*
          * Method to change the role of the database "reestablishes
@@ -48,29 +52,54 @@ class DB
         $this->ResetDefaultConnection($Rol);
     }
 
-    public function getConnection(){
+    /**
+     * @return mysqli
+     */
+    public function getConnection():mysqli{
+        if(!isset($this->connection)){
+            $this->ResetDefaultConnection();
+        }
         return $this->connection;
     }
 
     /**
      * @throws Exception
+     * @return DB
      */
     public static function getInstance():DB{
         if(!isset(self::$instance)){
             self::$instance = new self;
         }
-
         return self::$instance;
     }
 
-    private function __wakeup()
+    /**
+     * @throws Exception
+     */
+    public function __wakeup()
     {
-        //to prevent serialization
+        /*
+         * to prevent serialization.
+         * your visibility must be public
+         * */
+        throw new Exception('No puedes serializar');
     }
 
+    /**
+     * @throws Exception
+     */
     private function __clone()
     {
         //to prevent duplication
+        throw new Exception('No puedes clonar esta clase');
     }
 
+
+    /*
+     * Made in Colombia by:
+     * Carlos Daniel Castro Maussa
+     * Juan Guillermo Florez Burgos
+     * Daniela Salazar Gonzalez
+     * Sebastian Quinchia Lobo
+     * */
 }
