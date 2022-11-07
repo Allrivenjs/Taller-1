f9f9cf06a3d783e2374505d187ad27b573035b5d
Jortizuwu <joseher2511@gmail.com>
2022-11-07 12:00:12 -0500

    mail templates add

--- controllers/Email/EmailController.php
+++ controllers/Email/EmailController.php
@@ -5,17 +5,28 @@ namespace Controllers\Email;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
 
+
 class SendEmailController
 {
   /**
    * @param $userMail
    * @param $userName
    * @param $valueSent
+   * @param $accountType
+   * @param $typeTransfer
+   * @param $isReceiving
+   * @param $isFaild
    * @return string
    */
   public function sendEmail($userMail, $userName, $valueSent = "0", $accountType = "ahorro", $typeTransfer = "externa", $isReceiving = true, $isFaild = false)
   {
     try {
+
+      $message = file_get_contents("mail_templates/sample_mail.html");
+      $table = file_get_contents("mail_templates/table.html");
+
+      echo $message;
+
       // Intancia de PHPMailer
       $mail = new PHPMailer();
 
@@ -23,19 +34,19 @@ class SendEmailController
       $mail->isSMTP();
 
       //Set the hostname of the mail server
-      $mail->Host          = 'smtp.gmail.com';
-      $mail->Port          = 465;
+      $mail->Host = 'smtp.gmail.com';
+      $mail->Port = 465;
 
       // Propiedad para establecer la seguridad de encripción de la comunicación
-      $mail->SMTPSecure    = PHPMailer::ENCRYPTION_SMTPS;
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 
       // Para activar la autenticación smtp del servidor
-      $mail->SMTPAuth      = true;
+      $mail->SMTPAuth = true;
 
       // Credenciales de la cuenta
-      $email              = 'bancarapidanoreply@gmail.com';
-      $mail->Username     = $email;
-      $mail->Password     = 'twsmkayxanqoxnks';
+      $email = 'bancarapidanoreply@gmail.com';
+      $mail->Username = $email;
+      $mail->Password = 'twsmkayxanqoxnks';
 
       // Quien envía este mensaje
       $mail->setFrom($email, 'BANCA RAPIDA');
@@ -47,324 +58,31 @@ class SendEmailController
       $mail->Subject = 'BancaRapida le informa';
 
 
-      $resultado = $isReceiving ? "-" : "+";
-      $receiving = $isReceiving ? "dinero enviado" : "dinero recibido";
+      $receiving = $isReceiving ? "Dinero enviado" : "Dinero recibido";
       $faild = $isFaild ? "rechazada" : "exitosa";
       $faildTitle = $isFaild ? "Transaccion rechazada" : "Transaccion exitosa";
-      $faildTable = $isFaild ? "rechazada" : "<table
-      border='0'
-      style='
-        color: #000;
-        font-size: 13px;
-        line-height: 22px;
-        table-layout: auto;
-        width: 100%;
-      '
-    >
-      <tr
-        style='
-          border-bottom: 1px solid #ecedee;
-          text-align: left;
-        '
-      >
-        <th style='padding: 0 15px 10px 0'>Datos</th>
-        <th style='padding: 0 15px'></th>
-        <th style='padding: 0 0 0 15px' align='right'>
-          Dinero
-        </th>
-      </tr>
-      <tr>
-        <td style='padding: 5px 15px 5px 0'>" . $receiving . "</td>
-        <td style='padding: 0 15px'></td>
-        <td style='padding: 0 0 0 15px' align='right'>
-          " . $resultado . " $" . $valueSent . "
-        </td>
-      </tr>
-      <tr>
-        <td style='padding: 0 15px 5px 0'>Costo de la transaccion</td>
-        <td style='padding: 0 15px'></td>
-        <td style='padding: 0 0 0 15px' align='right'>
-          $0
-        </td>
-      </tr>
-      <tr>
-        <td style='padding: 0 15px 5px 0'>Fecha</td>
-        <td style='padding: 0 15px'></td>
-        <td style='padding: 0 0 0 15px' align='right'>
-          " . date('d F Y, h:i:s A') . "
-        </td>
-      </tr>
-      <tr
-        style='
-          border-bottom: 2px solid #ecedee;
-          text-align: left;
-          padding: 15px 0;
-        '
-      >
-        <td
-          style='padding: 5px 15px 5px 0; font-weight: bold'
-        >
-          TOTAL
-        </td>
-        <td style='padding: 0 15px'></td>
-        <td
-          style='padding: 0 0 0 15px; font-weight: bold'
-          align='right'
-        >
-           " . $resultado . "$" . $valueSent . "
-        </td>
-      </tr>
-    </table>";
+
+
+      $table = str_replace('%receiving%', $receiving, $table);
+      $table = str_replace('%valueSent%', $valueSent, $table);
+      $table = str_replace('%date%', date('d F Y, h:i:s A'), $table);
+
+      $faildTable = $isFaild ? "rechazada" : $table;
+
+      $message = str_replace('%userName%', $userName, $message);
+      $message = str_replace('%faildTitle%', $faildTitle, $message);
+      $message = str_replace('%accountType%', $accountType, $message);
+      $message = str_replace('%faild%', $faild, $message);
+      $message = str_replace('%typeTransfer%', $typeTransfer, $message);
+      $message = str_replace('%faildTable%', $faildTable, $message);
 
       // Contenido
       $mail->IsHTML(true);
       $mail->CharSet = 'UTF-8';
-      $mail->Body    = "
-            <!DOCTYPE html>
-            <html>
-              <head>
-                <title> </title>
-              </head>
-              <body style='background-color: #f9f9f9'>
-                <div style='background-color: #f9f9f9'>
-                  <div
-                    style='
-                      background: #f9f9f9;
-                      background-color: #f9f9f9;
-                      margin: 0px auto;
-                      max-width: 600px;
-                    '
-                  >
-                    <table
-                      align='center'
-                      border='0'
-                      cellpadding='0'
-                      cellspacing='0'
-                      role='presentation'
-                      style='background: #f9f9f9; background-color: #f9f9f9; width: 100%'
-                    >
-                      <tbody>
-                        <tr>
-                          <td
-                            style='
-                              border-bottom: #333957 solid 5px;
-                              direction: ltr;
-                              font-size: 0px;
-                              padding: 20px 0;
-                              text-align: center;
-                              vertical-align: top;
-                            '
-                          ></td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                
-                  <div
-                    style='
-                      background: #fff;
-                      background-color: #fff;
-                      margin: 0px auto;
-                      max-width: 600px;
-                    '
-                  >
-                    <table
-                      align='center'
-                      border='0'
-                      cellpadding='0'
-                      cellspacing='0'
-                      role='presentation'
-                      style='background: #fff; background-color: #fff; width: 100%'
-                    >
-                      <tbody>
-                        <tr>
-                          <td
-                            style='
-                              border: #dddddd solid 1px;
-                              border-top: 0px;
-                              direction: ltr;
-                              font-size: 0px;
-                              padding: 20px 0;
-                              text-align: center;
-                              vertical-align: top;
-                            '
-                          >
-                            <div
-                              class='mj-column-per-100 outlook-group-fix'
-                              style='
-                                font-size: 13px;
-                                text-align: left;
-                                direction: ltr;
-                                display: inline-block;
-                                vertical-align: bottom;
-                                width: 100%;
-                              '
-                            >
-                              <table
-                                border='0'
-                                cellpadding='0'
-                                cellspacing='0'
-                                role='presentation'
-                                style='vertical-align: bottom'
-                                width='100%'
-                              >
-                                <tr>
-                                  <td
-                                    align='center'
-                                    style='
-                                      font-size: 0px;
-                                      padding: 10px 25px;
-                                      word-break: break-word;
-                                    '
-                                  >
-                                    <table
-                                      align='center'
-                                      border='0'
-                                      cellpadding='0'
-                                      cellspacing='0'
-                                      role='presentation'
-                                      style='border-collapse: collapse; border-spacing: 0px'
-                                    >
-                                      <tbody>
-                                        <tr>
-                                        <td style='width: 150px'>
-                                        <img
-                                          height='auto'
-                                          src='https://cdn.discordapp.com/attachments/1027325078150594611/1028724631173083266/unknown.png'
-                                              style='
-                                                border: 0;
-                                                display: block;
-                                                outline: none;
-                                                text-decoration: none;
-                                                width: 100%;
-                                              '
-                                              width='64'
-                                            />
-                                          </td>
-                                        </tr>
-                                      </tbody>
-                                    </table>
-                                  </td>
-                                </tr>
-                
-                                <tr>
-                                  <td
-                                    align='center'
-                                    style='
-                                      font-size: 0px;
-                                      padding: 10px 25px;
-                                      word-break: break-word;
-                                    '
-                                  >
-                                    <div
-                                      style='
-                                        font-size: 24px;
-                                        font-weight: bold;
-                                        line-height: 22px;
-                                        text-align: center;
-                                        color: #525252;
-                                      '
-                                    >
-                                        " . $faildTitle . "
-                                    </div>
-                                  </td>
-                                </tr>
-                
-                                <tr>
-                                  <td
-                                    align='left'
-                                    style='
-                                      font-size: 0px;
-                                      padding: 10px 25px;
-                                      word-break: break-word;
-                                    '
-                                  >
-                                    <div
-                                      style='
-                                        font-size: 14px;
-                                        line-height: 22px;
-                                        text-align: left;
-                                        color: #525252;
-                                      '
-                                    >
-                                      <p>Hola " . $userName . "</p>
-                
-                                      <p>
-                                      Bancarapida le informa que la transaccion a su cuenta de " . $accountType . " fue " . $faild . ", 
-                                      acontinuacion vera los datos de la transaccion de tipo " . $typeTransfer . ",
-                                      gracias por confiar en nosotros.
-                                      </p>
-                                    </div>
-                                  </td>
-                                </tr>
-                
-                                <tr>
-                                  <td
-                                    align='left'
-                                    style='
-                                      font-size: 0px;
-                                      padding: 10px 25px;
-                                      word-break: break-word;
-                                    '
-                                  >
-                                    " . $faildTable . "
-                                  </td>
-                                </tr>
-                
-                                <tr>
-                                  <td
-                                    align='left'
-                                    style='
-                                      font-size: 0px;
-                                      padding: 10px 25px;
-                                      word-break: break-word;
-                                    '
-                                  ></td>
-                                </tr>
-                
-                                <tr>
-                                  <td
-                                    align='left'
-                                    style='
-                                      font-size: 0px;
-                                      padding: 10px 25px;
-                                      word-break: break-word;
-                                    '
-                                  >
-                                    <div
-                                      style='
-                                        font-size: 14px;
-                                        line-height: 20px;
-                                        text-align: left;
-                                        color: #525252;
-                                      '
-                                    >
-                                      BancaRapida<br />DW2<br />
-                                      <a
-                                        href='#'
-                                        style='color: #2f67f6'
-                                        >bancarapida.net</a
-                                      >
-                                    </div>
-                                  </td>
-                                </tr>
-                              </table>
-                            </div>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-                </body>
-            </html>
-            ";
+      $mail->MsgHTML($message);
 
       // Texto alternativo
       $mail->AltBody = 'BANCA RAPIDA LE INFORMA';
-
-      // Agregar algún adjunto
-      //$mail->addAttachment(IMAGES_PATH.'logo.png');
 
       // Enviar el correo
       if (!$mail->send()) {
@@ -377,3 +95,4 @@ class SendEmailController
     }
   }
 }
+?>
