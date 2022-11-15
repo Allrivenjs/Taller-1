eba7e5695f38b6c0de4ac0f0cf77335ee399a2ee
Milly <benitezmilly3@gmail.com>
2022-11-14 23:20:59 -0500

    Metodos de transferencias externas, falta método de getAll

---
+++ app/Controller/ExternalTransaction/ExternalTransactionController.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace App\Controller\ExternalTransaction;
+
+use Illuminate\Http\Request;
+use App\Database\Database;
+use mysqli;
+use mysqli_sql_exception;
+
+class ExternalTransactionController
+{
+
+    public function __construct()
+    {
+    }
+
+    public function CreateETE(): void
+    {
+        $request = Request::capture();
+        $ConDB = Database::getInstance()->getConnection();
+
+        $EANumber = mysqli_real_escape_string($ConDB, $request->post("ea_number"));
+        $verified = ExternalTransactionController::VerifiedAccount($EANumber);
+        //Variables que almacenaran los datos para la consulta
+        $idAccount = $verified;
+        $transactionType = mysqli_real_escape_string($ConDB, $request->post("transaction_type"));
+        $EAType = mysqli_real_escape_string($ConDB, $request->post("ea_type"));
+        $amount = intval(mysqli_real_escape_string($ConDB, $request->post("amount")));
+        $date = mysqli_real_escape_string($ConDB, $request->post("date"));
+        $status = 'aprove';
+        $EAOwnerName = mysqli_real_escape_string($ConDB, $request->post("eao_name"));
+        $EAOwnerId = mysqli_real_escape_string($ConDB, $request->post("eao_id"));
+        $EAOwnerIdType = mysqli_real_escape_string($ConDB, $request->post("eao_idtype"));
+        $description = mysqli_real_escape_string($ConDB, $request->post("description"));
+        $bankName = mysqli_real_escape_string($ConDB, $request->post("bank_name"));
+
+
+        if (strlen($verified) > 0) {
+            $res_amount = ExternalTransactionController::VerifiedAmount($EANumber);
+            $query_success = "INSERT into `externaltransfer` (`idAccount`, `EANumber`, `transactionType`, `EAType`, `amount`, `date`, `status`, `EAOwnerName`, `EAOwnerId`, `EAOwnerIdType`, `description`, `bankName`)  VALUES ('$idAccount','$EANumber','$transactionType','$EAType','$amount','$date','$status','$EAOwnerName','$EAOwnerId', '$EAOwnerIdType','$description','$bankName')";
+            $req = mysqli_query($ConDB, $query_success);
+
+            $new_amount = $res_amount + $amount;
+            ExternalTransactionController::UpdateAmount($idAccount, $new_amount);
+            http_response_code(200);
+            print(json_encode(array('message' => 'Transacción realizada con éxito')));
+            $ConDB->close();
+        } else {
+
+            http_response_code(404);
+            print(json_encode(array('message' => 'Número de cuenta no existe')));
+        }
+    }
+
+    public function CreateETI(): void
+    {
+        $request = Request::capture();
+        $ConDB = Database::getInstance()->getConnection();
+        $EANumber = mysqli_real_escape_string($ConDB, $request->post("ea_number"));
+        $res_amount = ExternalTransactionController::VerifiedAmount($EANumber);
+        $idAccount = '1';
+        $transactionType = mysqli_real_escape_string($ConDB, $request->post("transaction_type"));
+        $EAType = mysqli_real_escape_string($ConDB, $request->post("ea_type"));
+        $amount = mysqli_real_escape_string($ConDB, $request->post("amount"));
+        $date = mysqli_real_escape_string($ConDB, $request->post("date"));
+        $status = 'aprove';
+        $EAOwnerName = mysqli_real_escape_string($ConDB, $request->post("eao_name"));
+        $EAOwnerId = mysqli_real_escape_string($ConDB, $request->post("eao_id"));
+        $EAOwnerIdType = mysqli_real_escape_string($ConDB, $request->post("eao_idtype"));
+        $description = mysqli_real_escape_string($ConDB, $request->post("description"));
+        $bankName = mysqli_real_escape_string($ConDB, $request->post("bank_name"));
+        if ($res_amount > $amount) {
+            try {
+                $query_success = "INSERT INTO `externaltransfer` (`idAccount`, `EANumber`, `transactionType`, `EAType`, `amount`, `date`, `status`, `EAOwnerName`, `EAOwnerId`, `EAOwnerIdType`, `description`, `bankName`)  VALUES ('$idAccount','$EANumber','$transactionType','$EAType','$amount','$date','$status','$EAOwnerName','$EAOwnerId', '$EAOwnerIdType','$description','$bankName')";
+                $req = mysqli_query($ConDB, $query_success);
+
+                $new_amount = $res_amount - $amount;
+                ExternalTransactionController::UpdateAmount($idAccount, $new_amount);
+
+                //Respuesta
+                http_response_code(200);
+                print(json_encode(array('message' => 'Transacción realizada con éxito')));
+                $ConDB->close();
+
+            } catch (mysqli_sql_exception $e) {
+                //throw $th;$ConDB->rollback();
+                $ConDB->rollback();
+                http_response_code(409);
+                print($e);
+            } finally {
+                $ConDB->close();
+            }
+        } else {
+            try {
+                $status = 'refused';
+
+                $query_unsuccess = "INSERT INTO `externaltransfer` (`idAccount`, `EANumber`, `transactionType`, `EAType`, `amount`, `date`, `status`, `EAOwnerName`, `EAOwnerId`, `EAOwnerIdType`, `description`, `bankName`)  VALUES ('$idAccount','$EANumber','$transactionType','$EAType','$amount','$date','$status','$EAOwnerName','$EAOwnerId', '$EAOwnerIdType','$description','$bankName')";
+                $req = mysqli_query($ConDB, $query_unsuccess);
+                $ConDB->close();
+
+                http_response_code(200);
+                print(json_encode(array('message' => 'La transacción no se pudo realizar, no cuenta con fondos suficientes')));
+            } catch (mysqli_sql_exception $e) {
+                $ConDB->rollback();
+                http_response_code(409);
+                //print($e);
+            } finally {
+                $ConDB->close();
+            }
+        }
+    }
+
+    public function VerifiedAccount($id)
+    {
+
+        $response = '';
+        $request = Request::capture();
+        $ConDB = Database::getInstance()->getConnection();
+        $idAccount = mysqli_real_escape_string($ConDB, $request->post("id_account"));
+        $query  = "SELECT * FROM account where accountNumber = '$id'";
+        $req = mysqli_query($ConDB, $query);
+        $query_row = mysqli_fetch_array($req);
+
+        if ($query_row) {
+            $response = $query_row['id'];
+        }
+
+        return $response;
+    }
+
+    public function UpdateAmount($id, $amount)
+    {
+        $ConDB = Database::getInstance()->getConnection();
+        $query  = "UPDATE `account` SET `amount` = '$amount' where id = '$id'";
+        $req = mysqli_query($ConDB, $query);
+        $ConDB->close();
+    }
+    public function VerifiedAmount($id)
+    {
+        $response = 0;
+        $request = Request::capture();
+        $ConDB = Database::getInstance()->getConnection();
+        $idAccount = mysqli_real_escape_string($ConDB, $request->post("id_account"));
+        $query  = "SELECT * FROM account where accountNumber = '$id'";
+        $req = mysqli_query($ConDB, $query);
+        $query_row = mysqli_fetch_array($req);
+
+        if ($query_row) {
+            $response = $query_row['amount'];
+        }
+
+        return $response;
+    }
+
+    public function getAllET(){
+        $request = Request::capture();
+        $ConDB = Database::getInstance()->getConnection();
+        $query  = "SELECT * FROM account";
+        $req = mysqli_query($ConDB, $query);
+        $query_row = mysqli_fetch_array($req);
+
+        $myarry = [];
+        if ($query_row) {
+
+            try {
+                while($query_row)
+                {
+                  $info =  array('id' => $query_row['id'],'idAccount'=> $query_row['idAccount'],'EANumber'=> $query_row['EANumber'], 'transactioType' => $query_row['transactionType'], 'EAType'=> $query_row['EAType'], 'amount' => $query_row['amount'], 'date'=> $query_row['date'], 'status'=> $query_row['status'], 'EAOwnerName'=>$query_row['EAOwnerName'],'EAOwnerId' => $query_row['EAOwnerId'], 'EAOwnerTypeId'=>$query_row['EAOwnerIdType'],'description'=> $query_row['description'], 'bankName'=>$query_row['bankName']);
+
+                  print($query_row['EANumber']);
+                  //  array_push($myarry,$info);
+
+                }
+
+                print(json_encode(array('message' => 'Datos encontrados', 'data'=> $query_row)));
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+            
+            //print($query_row);
+        }
+    }
+
+    public function getIdET(){
+        $request = Request::capture();
+        $ConDB = Database::getInstance()->getConnection();
+        $idAccount = mysqli_real_escape_string($ConDB, $request->post("id_account"));
+
+        $query  = "SELECT * FROM externaltransfer where id = '$idAccount'";
+        $req = mysqli_query($ConDB, $query);
+        $query_row = mysqli_fetch_array($req);
+
+        if ($query_row) {
+            try {
+                  $info =  array('id' => $query_row['id'],'idAccount'=> $query_row['idAccount'],'EANumber'=> $query_row['EANumber'], 'transactioType' => $query_row['transactionType'], 'EAType'=> $query_row['EAType'], 'amount' => $query_row['amount'], 'date'=> $query_row['date'], 'status'=> $query_row['status'], 'EAOwnerName'=>$query_row['EAOwnerName'],'EAOwnerId' => $query_row['EAOwnerId'], 'EAOwnerTypeId'=>$query_row['EAOwnerIdType'],'description'=> $query_row['description'], 'bankName'=>$query_row['bankName']);
+                print(json_encode(array('message' => 'Datos encontrados', 'data'=> $info)));
+            } catch (\Throwable $th) {
+                //throw $th;
+                $ConDB->rollback();
+                http_response_code(409);
+            } finally{
+                $ConDB->close();
+
+            }
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Controller\Auth\AuthController;
 use Phroute\Phroute\RouteCollector;
+use App\Controller\ExternalTransaction\ExternalTransactionController;
 $router = Config\Providers\RouteServiceProviders::getInstance()->getRouter();
 
 $router->get('/', function () {
@@ -13,6 +14,12 @@ $router->group(['prefix'=> 'api'], function (RouteCollector $router){
 
     $router->post('/login', [AuthController::class, 'Login']);
     $router->post('/signout', [AuthController::class, 'SignOut']);
+
+    $router->get('/externalTransById', [ExternalTransactionController::class, 'getIdET']);
+    $router->post('/externalTransCreateExternal', [ExternalTransactionController::class, 'CreateETE']);
+    $router->post('/externalTransCreateInternal', [ExternalTransactionController::class, 'CreateETI']);
+
+
 
 
     $router->group(['before' => 'auth'], function(RouteCollector $router){
