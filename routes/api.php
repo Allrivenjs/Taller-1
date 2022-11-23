5ded769f643d7164759cfc83e357e2fd80038c28
Angel Kendall <kendallangel37@gmail.com>
2022-11-23 12:44:23 -0500

    Latest Driver Editions

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,17 @@
 
 use App\Controller\Auth\AuthController;
 use Phroute\Phroute\RouteCollector;
+use App\Controller\Credit\CreditController;
 $router = Config\Providers\RouteServiceProviders::getInstance()->getRouter();
 
 $router->get('/', function () {
     echo 'Hello World!';
+    
 });
+
+$router->get('/view/credit', [CreditController::class, 'ViewCredit']);
+$router->get('/view/payment', [CreditController::class, 'ViewPayment']);
+$router->post('/create/status', [CreditController::class, 'StatusCredit']);
 
 
 $router->group(['prefix'=> 'api'], function (RouteCollector $router){
@@ -16,6 +22,8 @@ $router->group(['prefix'=> 'api'], function (RouteCollector $router){
     $router->post('/signout', [AuthController::class, 'SignOut']);
 
     $router->group(['before' => 'auth'], function(RouteCollector $router){
+        $router->post('/create/credit', [CreditController::class, 'InsertCredit']);
+        
 
         $router->get('/posts', function(){
             echo 'posts';
